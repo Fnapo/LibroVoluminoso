ab67bafccb1de9409aebc404ac80d393c1acb29b
Fnapo <36490842+Fnapo@users.noreply.github.com>
2023-09-15 21:58:08 +0200

    Creación de la BBDD, codigo primero

---
+++ LibroVoluminoso/Data/DbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using LibroVoluminoso.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibroVoluminoso.Data
+{
+	public class AppDbContext : DbContext
+	{
+        public AppDbContext(DbContextOptions<AppDbContext> opciones) : base(opciones)
+        {
+            
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
---
+++ LibroVoluminoso/Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace LibroVoluminoso.Models
+{
+	public class Categoria
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string Nombre { get; set; }
+
+		public int MostrarOrden { get; set; }
+
+		public DateTime FechaCreacion { get; set; } = DateTime.Now;
+    }
+}
